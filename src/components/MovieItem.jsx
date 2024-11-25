ef566e0b39534106e5d7a34fd40b6d94620fc098
Omer4ruq <omerfaruq4@gmail.com>
2024-11-25 15:18:16 +0600

    scrollbar function added

--- src/components/MovieItem.jsx
+++ src/components/MovieItem.jsx
@@ -1,17 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+import { BiChevronLeft, BiChevronRight } from "react-icons/bi";
 
 const MovieItem = ({ movie }) => {
+  const [showArrows, setShowArrows] = useState(false);
   return (
-    <div>
-      <div className="relative w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block rounded-lg overflow-hidden cursor-pointer m-2 ">
+    <div
+      onMouseEnter={() => setShowArrows(true)}
+      onMouseLeave={() => setShowArrows(false)}
+      className="group"
+    >
+      <div className="relative w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px]  inline-block rounded-lg overflow-hidden cursor-pointer m-2 ">
         <img
           src={`https://image.tmdb.org/t/p/original/${movie?.backdrop_path}`}
           alt={movie.title}
           className="transition-transform duration-300 ease-in-out hover:scale-125"
         />
-        <h1 className="text-white text-base relative -mt-8">{movie.title}</h1>
+        <h1 className="text-white text-xs md:text-base  relative -mt-8 text-center">
+          {movie.title}
+        </h1>
         <h1 className=" text-white text-base relatve">{movie.duration}</h1>
       </div>
+      {/* {showArrows && (
+        <div>
+          {" "}
+          <button
+            className="absolute top-1/2 -translate-y-1/2 left-5 md:left-10 flex items-center flex justify-center
+        size-12 rounded-full bg-black bg-opacity-50 hover:bg-opacity-75 text-white z-10"
+          >
+            <BiChevronLeft size={24}></BiChevronLeft>
+          </button>
+          <button
+            className="absolute top-1/2 -translate-y-1/2 right-5 md:right-10 flex items-center flex justify-center
+        size-12 rounded-full bg-black bg-opacity-50 hover:bg-opacity-75 text-white z-10"
+          >
+            <BiChevronRight size={24}></BiChevronRight>
+          </button>
+        </div>
+      )} */}
     </div>
   );
 };
--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,26 +1,74 @@
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import MovieItem from "./MovieItem";
+import { BiChevronLeft, BiChevronRight } from "react-icons/bi";
 
 const Row = ({ title, fetchURL }) => {
-  console.log(title);
   const [movies, setMovies] = useState([]);
+  const [showArrows, setShowArrows] = useState(false);
+  const sliderRef = useRef(null);
 
+  // Scroll Left Function
+  const scrollLeft = () => {
+    if (sliderRef.current) {
+      sliderRef.current.scrollBy({
+        left: -sliderRef.current.offsetWidth,
+        behavior: "smooth",
+      });
+    }
+  };
+
+  // Scroll Right Function
+  const scrollRight = () => {
+    if (sliderRef.current) {
+      sliderRef.current.scrollBy({
+        left: sliderRef.current.offsetWidth,
+        behavior: "smooth",
+      });
+    }
+  };
+
+  // Fetch movies from API
   useEffect(() => {
     axios.get(fetchURL).then((res) => setMovies(res.data.results));
   }, [fetchURL]);
-  console.log(movies);
+
   return (
     <div>
       <h1 className="text-white font-bold md:text-xl p-4">{title}</h1>
-      <div className="">
-        <div
-          id={`slider`}
-          className="relative flex items-center w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide"
-        >
-          {movies.map((movie) => (
-            <MovieItem key={movie.id} movie={movie}></MovieItem>
-          ))}
+      <div
+        onMouseEnter={() => setShowArrows(true)}
+        onMouseLeave={() => setShowArrows(false)}
+      >
+        <div className="relative">
+          {/* Slider container */}
+          <div
+            id={`slider`}
+            ref={sliderRef}
+            className="flex items-center w-full h-full group overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide"
+          >
+            {movies.map((movie) => (
+              <MovieItem key={movie.id} movie={movie} />
+            ))}
+          </div>
+
+          {/* Arrows */}
+          {showArrows && (
+            <>
+              <button
+                className="absolute top-1/2 -translate-y-1/2 left-2 flex items-center justify-center w-8 h-8 rounded-full bg-black bg-opacity-50 hover:bg-opacity-75 text-white z-10"
+                onClick={scrollLeft}
+              >
+                <BiChevronLeft size={24} />
+              </button>
+              <button
+                className="absolute top-1/2 -translate-y-1/2 right-2 flex items-center justify-center w-8 h-8 rounded-full bg-black bg-opacity-50 hover:bg-opacity-75 text-white z-10"
+                onClick={scrollRight}
+              >
+                <BiChevronRight size={24} />
+              </button>
+            </>
+          )}
         </div>
       </div>
     </div>
