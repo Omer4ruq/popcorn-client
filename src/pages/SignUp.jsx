c95998498fc2bffaf208f12fe057e02188ade49d
Omer4ruq <omerfaruq4@gmail.com>
2024-12-13 23:45:19 +0600

    sign up page update

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const SignUp = () => {
-  return <div>Signup page</div>;
+  return (
+    <div>
+      Signup page
+      <h1>login with googe</h1>
+      <input type="text" />
+    </div>
+  );
 };
 
 export default SignUp;
